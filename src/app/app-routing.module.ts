df22a8d006a72d3dc45046b8dfddd0a5fabc2f49
edwardgazitonline <75829088+edwardgazitonline@users.noreply.github.com>
2023-05-04 10:03:04 +0300

    1 file modified

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Routes , RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+
+const routes: Routes   = [];
+
+@NgModule({
+  declarations: [AppComponent],
+  imports:[ BrowserModule , AppRoutingModule ],
+  providers:[],
+  bootstrap:[AppComponent],
+
+})
+
+
+export class AppRoutingModule {}
